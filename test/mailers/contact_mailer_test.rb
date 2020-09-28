d5657847d3f55f4186b4679b6a1e2e9d84246b07
Niitn <nitin.jain@kiwitech.com>
2020-09-28 14:25:54 +0530

    removed comments

--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -4,18 +4,13 @@ require 'test_helper'
 
 class ContactMailerTest < ActionMailer::TestCase
   test 'notify admin' do
-    # Create the email and store it for further assertions
     email = ContactMailer.notify_admin(contacts(:one))
-
-    # Send the email, then test that it got queued
     assert_emails 1 do
       email.deliver_now
     end
 
-    # Test the body of the sent email contains what we expect it to
     assert_equal ['from@example.com'], email.from
     assert_equal ['admin@example.com'], email.to
     assert_equal 'Someone has contacted you', email.subject
-    # assert_equal read_fixture('invite').join, email.body.to_s
   end
 end
